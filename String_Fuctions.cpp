6e81cc1266952b01de345b05b86426de91496e48
AdiSAnkolli <59432457+AdiSAnkolli@users.noreply.github.com>
2020-12-28 09:56:08 +0530

    Create String_Fuctions.cpp

---
+++ String_Fuctions.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main () {
+
+   char str1[10] = "Hello";
+   char str2[10] = "Loml";
+   char str3[10];
+   int  len ;
+
+   // copy str1 into str3
+   strcpy( str3, str1);
+   cout << "strcpy( str3, str1) : " << str3 << endl;
+
+   // concatenates str1 and str2
+   strcat( str1, str2);
+   cout << "strcat( str1, str2): " << str1 << endl;
+
+   // total lenghth of str1 after concatenation
+   len = strlen(str1);
+   cout << "strlen(str1) : " << len << endl;
+
+   return 0;
+}
+
+//OUTPUT
+/*
+strcpy( str3, str1) : Hello
+strcat( str1, str2): HelloLoml
+strlen(str1) : 10
+*/
+
+
+//Or we can do the same thing using 'string' datatype
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+
+   string str1 = "Hello";
+   string str2 = "World";
+   string str3;
+   int  len ;
+
+   // copy str1 into str3
+   str3 = str1;
+   cout << "str3 : " << str3 << endl;
+
+   // concatenates str1 and str2
+   str3 = str1 + str2;
+   cout << "str1 + str2 : " << str3 << endl;
+
+   // total length of str3 after concatenation
+   len = str3.size();
+   cout << "str3.size() :  " << len << endl;
+
+   return 0;
+}
+
+//OUTPUT
+str3 : Hello
+str1 + str2 : HelloLoml
+str3.size() :  10
