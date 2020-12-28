03c505cbe6a23dc16580785b202901b35f4b87ce
AdiSAnkolli <59432457+AdiSAnkolli@users.noreply.github.com>
2020-12-28 10:04:54 +0530

    Create InputOutput.cpp

---
+++ InputOutput.cpp
@@ -0,0 +1,19 @@
+// Most basic input and output methods
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+   char name[50];
+ 
+   cout << "Please enter your name: ";
+   cin >> name;
+   cout << "Your name is: " << name << endl;
+ 
+}
+
+//OUTPUT 
+/*
+Please enter your name: LalaFluffBunny
+Your name is: LalaFluffBunny
