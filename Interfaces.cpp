64cf93fbdb5df278d0a2a3852e6535b29d523e6b
AdiSAnkolli <59432457+AdiSAnkolli@users.noreply.github.com>
2020-12-28 12:25:47 +0530

    Create Interfaces.cpp

---
+++ Interfaces.cpp
@@ -0,0 +1,68 @@
+/*
+An interface describes the behavior or capabilities of a C++ class without committing to a particular implementation of that class.
+It's a part of abstraction and encapsulation itself, the interface which is available to the outside world is what this module focuses on:-
+*/
+
+//Sample Code:-
+
+#include <iostream>
+ 
+using namespace std;
+ 
+// Base class
+class Shape {
+   public:
+      // pure virtual function providing interface framework.
+      virtual int getArea() = 0;
+      void setWidth(int w) {
+         width = w;
+      }
+   
+      void setHeight(int h) {
+         height = h;
+      }
+   
+   protected:
+      int width;
+      int height;
+};
+ 
+// Derived classes
+class Rectangle: public Shape {
+   public:
+      int getArea() { 
+         return (width * height); 
+      }
+};
+
+class Triangle: public Shape {
+   public:
+      int getArea() { 
+         return (width * height)/2; 
+      }
+};
+ 
+int main(void) {
+   Rectangle Rect;
+   Triangle  Tri;
+ 
+   Rect.setWidth(5);
+   Rect.setHeight(7);
+   
+   // Print the area of the object.
+   cout << "Total Rectangle area: " << Rect.getArea() << endl;
+
+   Tri.setWidth(5);
+   Tri.setHeight(7);
+   
+   // Print the area of the object.
+   cout << "Total Triangle area: " << Tri.getArea() << endl; 
+
+   return 0;
+}
+
+//Output:-
+/*
+Total Rectangle area: 35
+Total Triangle area: 17
+*/
